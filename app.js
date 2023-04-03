b72adbfa8cc398ceef6c726f65a8415e16f2e97f
Jon Cramer <joncramer96@gmail.com>
2023-04-03 06:24:57 -0400

    alert banner

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 const alertBanner = document.getElementById("alert");
 alertBanner.innerHTML = 
 `<div class="alert-banner">
-    <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
+    <p class="alert-message"><strong>Alert:</strong> You have unread messages</p>
     <p class="alert-banner-close">x</p>
 </div> `;
 
