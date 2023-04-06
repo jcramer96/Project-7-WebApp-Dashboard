801cd30e0adfdd9b0d826b6251af5ed3109d06a7
Jon Cramer <joncramer96@gmail.com>
2023-04-06 06:25:20 -0400

    updated graphs for hour, day, week, month

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 /* ============================================= */
 
 const notificationBell = document.querySelector('.notification-bell');
-const notifications = document.getElementById('notifications');
+const notifications = document.querySelector('.notifications');
 notificationBell.addEventListener('mouseover', e => {
     if (e.target.classList.contains("notification-bell")) {
         notifications.style.display = "flex"
@@ -43,7 +43,25 @@ alertBanner.addEventListener('click', e => {
 
 const trafficCanvas = document.getElementById('traffic-chart');
 
-let trafficData = {
+let hourly = {
+    labels: ["1:00", "2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00", "10:00", "11:00"],
+    datasets: [{
+            data: [25, 30, 15, 35, 20, 45, 20, 15, 35, 15, 20],
+            backgroundColor: 'rgba(116, 119, 191, .3)',
+            borderWidth: 1,
+        }]
+    };
+
+let daily = {
+    labels: ["Wed", "Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+    datasets: [{
+            data: [225, 350, 300, 150, 250, 450, 300, 250, 400, 350, 200],
+            backgroundColor: 'rgba(116, 119, 191, .3)',
+            borderWidth: 1,
+        }]
+    };
+
+let weekly = {
     labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
     "4-10", "11-17", "18-24", "25-31"],
     datasets: [{
@@ -53,11 +71,24 @@ let trafficData = {
         }]
     };
 
+let monthly = {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov"],
+    datasets: [{
+            data: [10000, 10250, 9750, 11000, 11500, 12000, 9000, 10500, 10000, 10500, 11000],
+            backgroundColor: 'rgba(116, 119, 191, .3)',
+            borderWidth: 1,
+        }]
+    };
+
+let trafficData = hourly;
+
 const trafficNav = document.querySelector('.traffic-nav');
 
 trafficNav.addEventListener('click', (e) => {
     let element = e.target;
-    if (element.classList.contains('traffic-nav-link')) {
+    if (document.querySelector('#hourly') && element.classList.contains('traffic-nav-link')) {
+        element.classList.toggle('traffic-nav-link-selected');
+    } else if (document.querySelector('#daily') && element.classList.contains('traffic-nav-link')) {
         element.classList.toggle('traffic-nav-link-selected');
     }
 });
